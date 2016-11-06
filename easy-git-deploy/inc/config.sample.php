2a742faf25928096fbdffbc30fbd376ff5e7b899
MarkVachi <gm_37@hotmail.com>
2016-11-06 11:40:43 +0700

    Update config.sample.php
    one step pull

--- easy-git-deploy/inc/config.sample.php
+++ easy-git-deploy/inc/config.sample.php
@@ -4,7 +4,7 @@
 $config = array(
 
     // The directory where your website and git repository are located, can be a relative or absolute path
-    'directory' => '../my_repo',
+    'directory' => '../../',
 
     // Used for first clone
     'url' => 'https://USER:PASS@bitbucket.org/account/repo.git'
